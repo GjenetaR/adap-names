15d8ab607bf698eda1ff09848cdba5fb205dbde0
Gjeneta Ramandani <gjeneta@Air-von-Gjeneta.fritz.box>
2025-11-29 15:13:50 +0100

    copy implementations of superclass and subclasses from B03 submission

--- src/adap-b04/names/StringArrayName.ts
+++ src/adap-b04/names/StringArrayName.ts
@@ -7,63 +7,86 @@ export class StringArrayName extends AbstractName {
     protected components: string[] = [];
 
     constructor(source: string[], delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
+        super(delimiter);
+        if (!Array.isArray(source)) {
+            throw new Error("Constructor expects an array of components");
+        }
+
+        // Validate that all components are strings
+        for (const comp of source) {
+            if (typeof comp !== "string") {
+                throw new Error("All components must be strings");
+            }
+        }
+        // Copy components 
+        this.components = [...source];
     }
 
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        return new StringArrayName([...this.components], this.delimiter);
     }
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        return this.components.join(delimiter);
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
+            let res: string = '';
+                    for (let i = 0; i < this.components.length;  i++){
+                        if (i != 0){
+                            res += DEFAULT_DELIMITER;
+                        }
+                        for (let j = 0; j < this.components[i].length; j++){
+                            if ((this.components[i][j] == DEFAULT_DELIMITER) || (this.components[i][j] == ESCAPE_CHARACTER)){
+                                res += ESCAPE_CHARACTER + this.components[i][j];
+                            }
+                            else res += this.components[i][j];
+                        }
+                    }
+            return res;
+        }
 
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
 
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
+            return this.components.length;
+        }
+    
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        if (i < 0 || i >= this.components.length) {
+            throw new Error("Index out of bounds");
+        }
+        return this.components[i];
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    public setComponent(i: number, c: string): void {
+        if (i < 0 || i >= this.components.length) {
+            throw new Error("Index out of bounds");
+        }
+        this.components[i] = c;
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    public insert(i: number, c: string): void {
+        if (i < 0 || i > this.components.length) {
+            throw new Error("Index out of bounds");
+        }
+        this.components.splice(i, 0, c);
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    public append(c: string): void {
+        this.components.push(c);
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
+    public remove(i: number): void {
+        if (i < 0 || i >= this.components.length) {
+            throw new Error("Index out of bounds");
+        }
+        this.components.splice(i, 1);
     }
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        const count = other.getNoComponents();
+        for (let i = 0; i < count; i++) {
+            this.components.push(other.getComponent(i));
+        };
     }
 }
